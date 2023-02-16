a48fb0d9ee0bc1e428f26473bcb0f5a79402b80b
Victor Malhotra <victorrmalhotra@gmail.com>
2023-02-16 15:21:47 -0700

    Made features with trents help

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import EventProgramInfo from '../Components/EventProgramInfo'
 
-const Main = () => {
+const Main=() => {
+  const [clicked, setClicked]=useState(false);
   return (
-    <div className='flex flex-row '>
-      <div>
+    <div className='grid sm:grid-cols-[6fr_4fr] grid-rows-1 min-h-screen'>
+      <div className='bg-pink-400' onClick={()=>setClicked(!clicked)}>
         Main
       </div>
-      <div>
-      <EventProgramInfo />
+      <div onClick={()=>setClicked(false)} className={`fixed sm:static sm:block bg-green-400 sm:w-full min-h-screen w-auto top-1/4 p-4 duration-500 transition ${clicked? 'translate-y-0':'translate-y-full'}`}>
+      <EventProgramInfo className="border-2 border-black border-solid" />
       </div>
     </div>
   )
