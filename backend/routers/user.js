86ca7839e6b559317805554a578f743a439dc5b2
B1gChungus <lruben095@west-mec.org>
2023-02-21 15:21:13 -0700

    completed user router

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,15 +1,15 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
-const app=express.Router()
-const {getUserByEmail,userJoin}=require('../Modules/user')
+const app = express.Router()
+const { getUserByEmail, userJoin } = require('../Modules/user')
 
 
 
-app.post("/join",async(req,res)=>{
-    const {userEmail,userName}=JSON.parse(req.body)
-    const output=await userJoin(userName,userEmail);
+app.post("/join", async (req, res) => {
+    const { Filter, userData } = req.body
+    const output = await userJoin(Filter, userData);
     res.send(output)
 })
 
 
-module.exports=app
+module.exports = app
