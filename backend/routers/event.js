f4a8ba12d79ecf3c8e75975ad19e41c389aa92a8
B1gChungus <lruben095@west-mec.org>
2023-02-21 14:00:22 -0700

    event creation

--- backend/routers/event.js
+++ backend/routers/event.js
@@ -1,40 +1,42 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
-const app=express.Router()
-const {addToEvent,endEvent,createEvent,getEvent,getParticipants}=require("../Modules/event")
-const {getParticipantData}=require("../Modules/user")
-const {sendEmail}=require('../Modules/email')
+const app = express.Router()
+const { addToEvent, endEvent, createEvent, getEvent, getParticipants } = require("../Modules/event")
+const { getParticipantData } = require("../Modules/user")
+const { sendEmail } = require('../Modules/email')
 //should block the sender from sending without meeting certain requirements to be a host
-app.use("/",async(req,res,next)=>{
+app.use("/", async (req, res, next) => {
     next();
 })
 
 
 
 //creates a new event into the database
-app.post("/createEvent",async(req,res)=>{
-    const {eventData}=await JSON.parse(req.body);
+app.post("/createEvent", async (req, res) => {
+    console.log(req.body)
+    const NewEvent = createEvent(req.body)
+    res.status(200).send({ message: "living failure" })
 })
 
 //returns the current event and it's data
-app.post("/getEvent",async(req,res)=>{
-    
-    res.send({success:true,event:await getEvent()})
+app.post("/getEvent", async (req, res) => {
+
+    res.send({ success: true, event: await getEvent() })
 })
 
-app.post("/updateEvent",async(req,res)=>{
-    
+app.post("/updateEvent", async (req, res) => {
+
 })
 
 //removes the event after compiling the event data to send as an email to the event host
-app.post("/finishEvent",async(req,res)=>{
-    const eventData=await getEvent();
-    const participantData=await getParticipantData(eventData.participants);
-    var participantFormat=participantData.map((participant)=>`<li><p>${participant.name}</p><p>${participant.email}</p><p>${eventData.eventName}</p></li>`).join("")
-    sendEmail("rgrang816@west-mec.org","testing",participantFormat);
+app.post("/finishEvent", async (req, res) => {
+    const eventData = await getEvent();
+    const participantData = await getParticipantData(eventData.participants);
+    var participantFormat = participantData.map((participant) => `<li><p>${participant.name}</p><p>${participant.email}</p><p>${eventData.eventName}</p></li>`).join("")
+    sendEmail("rgrang816@west-mec.org", "testing", participantFormat);
     endEvent();
 })
 
 
 
-module.exports=app
+module.exports = app
