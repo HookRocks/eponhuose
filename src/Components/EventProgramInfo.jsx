bb87cacb447a53a48fd2d3451457b80e0d50cb27
Victor Malhotra <victorrmalhotra@gmail.com>
2023-02-21 15:22:54 -0700

    add tabing

--- src/Components/EventProgramInfo.jsx
+++ src/Components/EventProgramInfo.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
-const EventProgramInfo = () => {
-  return (
-    <>
-    <div>EventProgramInfo</div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, itaque?</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, itaque?</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, itaque?</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, itaque?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, itaque?</p>
-      </>
-  )
+const EventProgramInfo=({tabbedMode}) => {
+  if(tabbedMode){
+    return (
+      <div className='flex align-middle items-center justify-center min-h-full'>
+        <h1 className='transform rotate-[270deg]'>
+          EventProgramInfo
+          </h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        EventProgramInfo
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, maiores.</p>
+      </div>
+    );
+  }
 }
 
 export default EventProgramInfo
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -19,14 +19,11 @@ const Main=() => {
   
   return (
     <div className={`grid sm:grid-cols-[6fr_4fr] grid-rows-1 min-h-screen lg:${clicked?'grid-cols-[9fr_1fr] grid-rows-1 min-h-screen':'grid-cols-[6fr_4fr] grid-rows-1 min-h-screen'} `}>
-      <div className={windowWidth<640?`bg-pink-400`:`bg-pink-400 w-full`} onClick={()=>setClicked(!clicked)}>
+      <div className={windowWidth<640? `bg-pink-400`:`bg-pink-400 w-full`} onClick={() => setClicked(!clicked)}>
         Main
       </div>
       <div onClick={() => setClicked(!clicked)} className={windowWidth<640?`fixed sm:static sm:block bg-green-400 w-full min-h-screen w-auto top-1/4 p-4 duration-500 transition ${clicked? 'translate-y-[33rem]':'translate-y-[-8rem]'}` : `static bg-green-400 min-h-screen w-full p-4 duration-500 transform` } >
-        <div>
-      <h2>Width: {windowWidth}</h2>
-    </div>
-      <EventProgramInfo className="border-2 border-black border-solid" />
+        <EventProgramInfo className="border-2 border-black border-solid" tabbedMode={clicked?true:false} />
       </div>
     </div>
   )
