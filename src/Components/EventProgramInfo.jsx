a4fc8b7159d5c9993b43aa77f3f60a30a32f3acd
Victor Malhotra <victorrmalhotra@gmail.com>
2023-02-22 14:35:28 -0700

    Make slidinga dn tabbing and design features work

--- src/Components/EventProgramInfo.jsx
+++ src/Components/EventProgramInfo.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const EventProgramInfo=({tabbedMode}) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
   if(tabbedMode){
     return (
-      <div className='flex align-middle items-center justify-center min-h-full'>
-        <h1 className='transform rotate-[270deg]'>
+      <div className={'flex align-middle items-center justify-center min-h-full font-extrabold text-2xl'}>
+        <h1 className={windowWidth<640?``:`transform rotate-[270deg]`}>
           EventProgramInfo
           </h1>
       </div>
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -18,9 +18,9 @@ const Main=() => {
   });
   
   return (
-    <div className={`grid sm:grid-cols-[6fr_4fr] grid-rows-1 min-h-screen lg:${clicked?'grid-cols-[9fr_1fr] grid-rows-1 min-h-screen':'grid-cols-[6fr_4fr] grid-rows-1 min-h-screen'} `}>
-      <div className={windowWidth<640? `bg-pink-400`:`bg-pink-400 w-full`} onClick={() => setClicked(!clicked)}>
-        Main
+    <div className={windowWidth<640?`grid sm:grid-cols-[6fr_4fr] grid-rows-1 min-h-screen`:`grid  lg:${clicked?'grid-cols-[9fr_1fr] grid-rows-1 min-h-screen':'grid-cols-[6fr_4fr] grid-rows-1 min-h-screen'} `}>
+      <div className={windowWidth<640? `${clicked?'':'flex text-center justify-center min-w-full font-extrabold text-2xl '} bg-pink-400 `:`bg-pink-400 w-full`} onClick={() => setClicked(!clicked)}>
+        Map
       </div>
       <div onClick={() => setClicked(!clicked)} className={windowWidth<640?`fixed sm:static sm:block bg-green-400 w-full min-h-screen w-auto top-1/4 p-4 duration-500 transition ${clicked? 'translate-y-[33rem]':'translate-y-[-8rem]'}` : `static bg-green-400 min-h-screen w-full p-4 duration-500 transform` } >
         <EventProgramInfo className="border-2 border-black border-solid" tabbedMode={clicked?true:false} />
