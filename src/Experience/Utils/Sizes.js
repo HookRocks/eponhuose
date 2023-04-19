c1fa3d19218bc7372effd819fdf67477da64514e
Ragingfury6 <trentsuperman@gmail.com>
2023-04-19 13:58:05 -0700

    All Locations Work

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -1,18 +1,26 @@
 import EventEmitter from 'events';
+import Experience from '../Experience';
 export default class Sizes extends EventEmitter {
   constructor() {
     super();
-    this.width = window.innerWidth;
+    this.currentSize = 0.9;
+    this.width = window.innerWidth * 0.9;
     this.height = window.innerHeight;
     this.aspect = this.width / this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
-
-    window.addEventListener('resize', () => {
-      this.width = window.innerWidth;
-      this.height = window.innerHeight;
-      this.aspect = this.width / this.height;
-      this.pixelRatio = Math.min(window.devicePixelRatio, 2);
-      this.emit('resize');
+    document.addEventListener('resizeEvent', () => {
+      this.currentSize = this.currentSize == 0.9 ? 0.6 : 0.9;
+      this.resize(this.currentSize);
     });
+    window.addEventListener('resize', () => {
+      this.resize();
+    });
+  }
+  resize(size = 0.9) {
+    this.width = window.innerWidth * size;
+    this.height = window.innerHeight;
+    this.aspect = this.width / this.height;
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+    this.emit('resize');
   }
 }
