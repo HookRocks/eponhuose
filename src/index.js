743856a0e74a530cc62581aa02b61d36ab497262
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2023-05-23 15:12:52 -0700

    i found the problem i think

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import Main from './Pages/Main.jsx';
 import AdminPage from './Pages/AdminPage';
 import {MarkerProvider} from './Contexts/MarkerContext';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 const router = createBrowserRouter([
   {
@@ -13,19 +13,15 @@ const router = createBrowserRouter([
     element: <Main />,
   },
   {
-    path: '/Admin',
+    path: '/admin',
     element: <AdminPage />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <React.StrictMode>
     <MarkerProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="Admin" element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
+      <RouterProvider router={router} />
     </MarkerProvider>
+  </React.StrictMode>
 );
