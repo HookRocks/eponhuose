1cf23b6d5f9dbc3303a57e6679ba59f3b799b7d1
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2023-05-23 14:56:24 -0700

    a

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
+  <React.StrictMode>
     <MarkerProvider>
       <RouterProvider router={router} />
     </MarkerProvider>
+  </React.StrictMode>
 );
